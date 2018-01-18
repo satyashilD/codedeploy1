b28bad81edfdabbbae4f703c881de3615df8d3f4
satyashildeshpande <33680995+satyashildeshpande@users.noreply.github.com>
2018-01-18 15:17:24 +0530

    Create instance_count.sh

---
+++ instance_count.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+running= $(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" | grep running | wc -l)
+stopped= aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped" | grep stopped | wc -l
+
+
+curl --data "body=Total Running Instances:$running<br> Total Stopped Instances:$stopped <br>&to=sachin.kale@adcolony.com &subject=Todays Instance Status " http://yourdomain.com/email/send
